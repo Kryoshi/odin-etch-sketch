8e22512aaad6ce5ed3f7d148d1fa9fb3f84053fb
Ashutush Choudhury <63922550+Kryoshi@users.noreply.github.com>
2024-04-20 21:21:46 +0530

    Add random color functionality, with progressive darkening

--- docs/script.js
+++ docs/script.js
@@ -31,13 +31,45 @@ function generateGrid() {
     }
     grid.addEventListener("mouseover", function (e) {
         if (e.target.className === "square") {
-            let currentOpacity = Number(e.target.style.opacity);
-            if (currentOpacity < 1) {
-                currentOpacity += 0.1;
-                e.target.setAttribute("style", `opacity: ${currentOpacity};`)
-            }
+            randomizeColor(e.target);
         }
     });
+}
+
+function randomizeColor (square) {
+    let r, g, b;
+    r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+    a = alpha(square.style.backgroundColor);
+    square.setAttribute("style", `background-color: rgb(${r}, ${g}, ${b}, ${a});`);
+}
+
+function alpha (color) {
+    let a = 0;
+    if (color === undefined || color === "") {
+        a = 0.1;
+    } else if (color) {
+        const rgbCheckArray = color.split("(")
+        if (rgbCheckArray[0] === "rgba") {
+            const colorArray = color.split(",");
+            let colorString = colorArray[colorArray.length - 1].trim();
+            let alphaString = "";
+            for (let i = 0; i < colorString.length; ++i) {
+                let currentChar = colorString.charAt(i);
+                if (currentChar == '.' || !isNaN(Number(currentChar))) {
+                    alphaString += currentChar;
+                }
+            }
+            a = Number(alphaString);
+            if (a < 1) {
+                a += 0.1;
+            }
+        } else {
+            a = 1;
+        }
+    }
+    return a;
 }
 
 newGridButton.addEventListener("click", generateGrid);
